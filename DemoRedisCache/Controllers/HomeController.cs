84a37f5f27064ed12e3ac012a49c9bc5364fe178
D24809 <darshan.naik@eon.com>
2016-07-12 10:06:08 +0100

    first checkin added redis for session management

---
+++ DemoRedisCache/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoRedisCache.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Session["RedisData"] = "This is stored in Redis Server , time - " + DateTime.Now.ToLongTimeString();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
